0c27026f55934b29c21a4aa3b44e6d4c09670cc6
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-07-18 03:51:09 -0500

    Adding more mm umls experiments

--- scripts/train.sh
+++ scripts/train.sh
@@ -4,9 +4,14 @@ python spert.py train --config=configs/mm_umls_train_v11.conf
 python spert.py train --config=configs/mm_umls_train_v12.conf
 python spert.py train --config=configs/mm_umls_train_v13.conf
 
+mv data/save/mm-umls-train-st21pv-v11/*/final_model data/save/mm-umls-train-st21pv-v11/final_model/
+mv data/save/mm-umls-train-st21pv-v12/*/final_model data/save/mm-umls-train-st21pv-v12/final_model/
+mv data/save/mm-umls-train-st21pv-v13/*/final_model data/save/mm-umls-train-st21pv-v13/final_model/
+
 python spert.py train --config=configs/mm_umls_i2b2_2010_train_v11.conf
 python spert.py train --config=configs/mm_umls_i2b2_2010_train_v12.conf
 python spert.py train --config=configs/mm_umls_i2b2_2010_train_v13.conf
 
 python spert.py train --config=configs/i2b2_2010_baseline_train.conf
 python spert.py train --config=configs/i2b2_2010_baseline_base_train.conf
+
