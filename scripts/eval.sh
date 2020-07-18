c7374b10d0e3ab5f70495a298e11e35c58922fc4
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-07-18 16:53:41 -0500

    Adding more mm umls experiments

---
+++ scripts/eval.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+python spert.py eval --config=configs/mm_umls_i2b2_2010_train_v11.conf
+python spert.py eval --config=configs/mm_umls_i2b2_2010_train_v12.conf
+python spert.py eval --config=configs/mm_umls_i2b2_2010_train_v13.conf
+
+python spert.py eval --config=configs/i2b2_2010_baseline_eval.conf
+python spert.py eval --config=configs/i2b2_2010_baseline_base_train.conf
--- scripts/train.sh
+++ scripts/train.sh
@@ -15,3 +15,12 @@ python spert.py train --config=configs/mm_umls_i2b2_2010_train_v13.conf
 python spert.py train --config=configs/i2b2_2010_baseline_train.conf
 python spert.py train --config=configs/i2b2_2010_baseline_base_train.conf
 
+mv data/save/mm-umls-i2b2-2010-train-v11/*/final_model data/save/mm-umls-i2b2-2010-train-v11/final_model/
+mv data/save/mm-umls-i2b2-2010-train-v12/*/final_model data/save/mm-umls-i2b2-2010-train-v12/final_model/
+mv data/save/mm-umls-i2b2-2010-train-v13/*/final_model data/save/mm-umls-i2b2-2010-train-v13/final_model/
+
+
+mv data/save/i2b2-2010-baseline-train-v1/*/final_model data/save/i2b2-2010-baseline-train-v1/final_model/
+mv data/save/i2b2-2010-baseline-base-train-v1/*/final_model data/save/i2b2-2010-baseline-base-train-v1/final_model/
+
+python spert.py eval --config=configs/
