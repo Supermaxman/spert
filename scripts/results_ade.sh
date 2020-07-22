89549016a376c0cb6e02ab31fefa0e123a4afc71
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-07-22 11:23:50 -0500

    Added ade experiment configs

--- scripts/results_ade.sh
+++ scripts/results_ade.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # $1 name of experiment, eg biobert
-for split in {0..9}; do
-    cat data/results/ade-$1-${split}-*-eval-test.csv | awk 'NR % 2 == 0'
+for seed in {0..4}; do
+    cat data/results/ade-$1-*-${seed}-eval-test.csv | awk 'NR % 2 == 0'
 done
