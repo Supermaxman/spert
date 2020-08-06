96167f721961664ac508280bf1959f994c80f8a8
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-08-05 22:36:16 -0500

    added Bio_Discharge_Summary_BERT

--- scripts/train_and_eval.sh
+++ scripts/train_and_eval.sh
@@ -14,15 +14,15 @@ python spert.py train \
   --train_path=$4 \
   --valid_path=$5
 
-mv data/save/$1-$3-train/*/final_model data/save/$1-$3-train/final_model/
+mv /users/max/data/models/spert/save/$1-$3-train/*/final_model /users/max/data/models/spert/save/$1-$3-train/final_model/
 
 python spert.py eval \
  --label=$1-$3-eval \
  --config=$2-eval.conf \
  --seed=$3 \
- --model_path=data/save/$1-$3-train/final_model/ \
- --tokenizer_path=data/save/$1-$3-train/final_model/ \
+ --model_path=/users/max/data/models/spert/save/$1-$3-train/final_model/ \
+ --tokenizer_path=/users/max/data/models/spert/save/$1-$3-train/final_model/ \
  --dataset_path=$6
 
-mv data/log/$1-$3-eval/*/* data/log/$1-$3-eval/
-cp data/log/$1-$3-eval/eval_test.csv data/results/$1-$3-eval-test.csv
+mv /shared/hltdir4/disk1/max/logs/spert/$1-$3-eval/*/* /shared/hltdir4/disk1/max/logs/spert/$1-$3-eval/
+cp /shared/hltdir4/disk1/max/logs/spert/$1-$3-eval/eval_test.csv data/results/$1-$3-eval-test.csv
