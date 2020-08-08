e0661289848157a83867bb1a2a00072dabce42a8
Maxwell Weinzierl <supermaxman1@gmail.com>
2020-08-07 21:12:10 -0500

    added compare script

--- spert/postprocess/compare_predictions.py
+++ spert/postprocess/compare_predictions.py
@@ -32,13 +32,45 @@ def compare(label, pred):
 	return entity_correct and rel_correct
 
 
-def compare_results(label_list, correct_pred_list, incorrect_pred_list, correct_model_list, incorrect_model_list):
+if __name__ == '__main__':
+	arg_parser = argparse.ArgumentParser()
+
+	arg_parser.add_argument(
+		'--label_path', type=str, help="Path to labels", default='data/datasets/ade/ade_split_1_test.json')
+	arg_parser.add_argument('--model_path', type=str, default='/shared/hltdir4/disk1/max/logs/spert/')
+	arg_parser.add_argument('--output_path', type=str, default='results/ade-results.json')
+	arg_parser.add_argument('--seed', type=str, default=1)
+	arg_parser.add_argument('--split', type=str, default=1)
+	arg_parser.add_argument('--require_relations', type=bool, default=True)
+	arg_parser.add_argument(
+		'--correct_model_list', type=str, help="List of model which get sentence right.", default='ade-biobert')
+	arg_parser.add_argument(
+		'--incorrect_model_list', type=str, help="List of models which get sentence wrong.", default='ade-bert-base')
+
+	args = arg_parser.parse_args()
+
+	label_path = args.label_path
+	model_path = args.model_path
+	output_path = args.output_path
+	seed = args.seed
+	split = args.split
+	require_relations = args.require_relations
+	correct_model_list = [format_models(name) for name in args.correct_model_list.split(',')]
+	incorrect_model_list = [format_models(name) for name in args.incorrect_model_list.split(',')]
+
+	labels = json.load(open(label_path))
+	correct_preds = [load_model_predictions(model_path, full_model_name) for full_model_name in correct_model_list]
+	incorrect_preds = [load_model_predictions(model_path, full_model_name) for full_model_name in incorrect_model_list]
+
 	results = []
-	for label, correct_preds, incorrect_preds in zip(label_list, zip(*correct_pred_list), zip(*incorrect_pred_list)):
+	for label, correct_preds, incorrect_preds in zip(labels, zip(*correct_preds), zip(*incorrect_preds)):
 		match = True
+		if len(label['relations']) == 0 and require_relations:
+			continue
 		result = {
 			'label': label
 		}
+
 		for i_pred, i_name in zip(incorrect_preds, incorrect_model_list):
 			match = match and not compare(label, i_pred)
 			result[i_name] = i_pred
@@ -49,38 +81,6 @@ def compare_results(label_list, correct_pred_list, incorrect_pred_list, correct_
 			del c_pred['tokens']
 		if match:
 			results.append(result)
-	return results
-
-
-if __name__ == '__main__':
-	arg_parser = argparse.ArgumentParser()
-
-	arg_parser.add_argument(
-		'--label_path', type=str, help="Path to labels", default='/users/max/data/corpora/i2b2/2010/json/test.json')
-	arg_parser.add_argument('--model_path', type=str, default='/shared/hltdir4/disk1/max/logs/spert/')
-	arg_parser.add_argument('--output_path', type=str, default='results/results.json')
-	arg_parser.add_argument('--seed', type=str, default=1)
-	arg_parser.add_argument('--split', type=str, default=None)
-	arg_parser.add_argument(
-		'--correct_model_list', type=str, help="List of model which get sentence right.", default='i2b2-biobert')
-	arg_parser.add_argument(
-		'--incorrect_model_list', type=str, help="List of models which get sentence wrong.", default='i2b2-bert-base')
-
-	args = arg_parser.parse_args()
-
-	label_path = args.label_path
-	model_path = args.model_path
-	output_path = args.output_path
-	seed = args.seed
-	split = args.split
-	correct_model_list = [format_models(name) for name in args.correct_model_list.split(',')]
-	incorrect_model_list = [format_models(name) for name in args.incorrect_model_list.split(',')]
-
-	labels = json.load(open(label_path))
-	correct_preds = [load_model_predictions(model_path, full_model_name) for full_model_name in correct_model_list]
-	incorrect_preds = [load_model_predictions(model_path, full_model_name) for full_model_name in incorrect_model_list]
-
-	results = compare_results(labels, correct_preds, incorrect_preds, correct_model_list, incorrect_model_list)
 
 	with open(output_path, 'w') as f:
 		json.dump(results, f, indent=2)
